75d88dd4892559d4d5c0bf76714588405860f0b2
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-02 20:41:33 -0600

    Implemented the state tree and a way to get the state tree
    we started building out the createStore function. Currently, this factory function:
    
    - takes in no arguments
    - sets up a local (private) variable to hold the state
    - sets up a getState() function
    - returns an object that publicly exposes the getState() function

--- app.js
+++ app.js
@@ -10,4 +10,17 @@ the store has the following information:
 */
 
 
+function createStore () {
+
+  let state  //this variable will hold the state of our entire application
+
+  const getState = () => state //the arrow function is responsible for returning the state of our app. Remember arror function are expressions
+
+  //When the user inovoke the createStore function they get an object back
+  //In order to access the internal state of our store they can ivoke the getState method
+  return {
+    getState
+  }
+}
+
 
