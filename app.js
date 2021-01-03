61cee648b080fbf9205094a35e0d8060daa481b1
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-03 00:37:17 -0600

    Updating State
    In summary, you will have
    - CreateStore() which will be a library that helps increase the predictablibility of state in your app to avoid bugs in relation to state management because it is all in one place
    -reducer function which you will make yourself that needs to be a pure function that improves the predictabiliity reducer function
    - Action that explains the event that occured

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ the store has the following information:
 - a way to update the state
 */
 
-
-function createStore () {
+//Library Code: In real Redux app, you will have an npm library that will do all this implementations below for you. So dont have to necessarily do this yourself
+//All you have to do is, pass to the library your reducer(App code) that is a pure function! and it will efficiently update the 
+//state tree for you
+function createStore (reducer) {
 
   let state  //this variable will hold the state of our entire application
   /*
@@ -33,11 +35,18 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    //Loop through all the listeners so that all the React Components that subscribe to this store can effectively update their UI 
+    listeners.forEach((listener) => listener())
+  }
+
   //When the user inovoke the createStore function they get an object back
   //In order to access the internal state of our store they can ivoke the getState method
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   }
 }
 
@@ -48,7 +57,7 @@ function createStore () {
  */
 
 
-const store = createStore()
+const store = createStore(todos) //you pass to the library your reducer. So that it will effectively know how to update the state tree
 
 //The user can inovke a subcribe method and pass a call back function. This callbackfunction when invoked by the user,
 //will listen for changes in the state tree as well as the user can do anything they want in this call back function
@@ -61,8 +70,58 @@ const unSubscribe = store.subscribe(() => {
   console.log('The store is created')
 })
 
-unSubscribe()
+// unSubscribe() if you want the component to unsubscribe from the store
 
 
+// App Code: This will be your App code in real Redux app
+//This code is a reducer method that explains how the state will change based on the action that occurs in the application
+//Reducer function MUST be a pure function
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+/*Whenever, you want to update the state, you  call the dispatch and pass the action object
+and the createStore will know how to update the state because of the reduer function
+Updates to the store can only be triggered by dispatching actions*/
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
+
+
+
+/*
+Pure functions must
+- Return the same result if the same arguments are passed in
+- Depend solely on the arguments passed into them
+- Do not produce side effects, such as API requests and I/O operations
+Eg of Pure functions
+const square = x => x * x;
+const double = array => {
+    let doubledArray = [];
+    array.forEach(n => doubledArray.push(n * 2));
+    return doubledArray;
+};
+
+eg of not pure functions
+
+const sumAndPrint = (a, b) => {
+    const sum = a = b;
+    console.log(sum)
+    return suml
+};
+
+Date.now()
+
+Math.random()
+ */
 
 
