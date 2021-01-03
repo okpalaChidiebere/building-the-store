1c9021c3f79152daac93796b8f812ea2b11747bc
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-02 20:38:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+/*
+the store has the following information:
+
+- the state tree
+- provide an API or a way to get the state tree
+- a way to listen and respond to the state changing
+- a way to update the state
+*/
+
+
+
